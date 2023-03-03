383890e62446647667ab48f26693ce3ad1974f4a
vorxsaken <jhonyevans6326@gmail.com>
2023-03-03 19:18:30 +0700

    chore: link to quiz

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className='container container--title container__block text-white text-medium'>
+        <Link href="/quiz">to quiz</Link>
+      </div>
     </div>
   )
 }
