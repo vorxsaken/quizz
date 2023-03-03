4ffd94926916af6d162de1abb685f11dee5b2b44
vorxsaken <jhonyevans6326@gmail.com>
2023-03-03 19:12:03 +0700

    chore: move backgroound logic from _app to indivisual component

---
+++ components/Background.js
@@ -0,0 +1,54 @@
+import blob from "../assets/blob.png"
+import blob2 from "../assets/blob2.png";
+import blob3 from "../assets/blob3.png";
+import blob4 from "../assets/blob4.png";
+import Image from 'next/image';
+import { useIsomorphicEffect } from "@/animation/useIsomophicEffect";
+import { gsap } from 'gsap';
+
+export default function Background() {
+    
+    useIsomorphicEffect(() => {
+        const timeLine = gsap.timeline({ repeat: -1, repeatDelay: 0.5, yoyo: true });
+        timeLine.to('#blob1', {
+            x: Math.floor(Math.random() * 800),
+            y: Math.floor(Math.random() * 500),
+            rotate: 360,
+            duration: 10,
+            ease: "power1.inOut"
+        })
+
+        timeLine.to('#blob2', {
+            x: Math.floor(Math.random() * 1000),
+            y: -Math.floor(Math.random() * 800),
+            rotate: 360,
+            duration: 15,
+            ease: "power1.inOut"
+        }, '<')
+
+        timeLine.to('#blob3', {
+            x: Math.floor(Math.random() * 500),
+            y: -Math.floor(Math.random() * 1000),
+            rotate: 360,
+            duration: 20,
+            ease: "power1.inOut"
+        }, '<')
+
+        timeLine.to('#blob4', {
+            x: Math.floor(Math.random() * 800),
+            y: -Math.floor(Math.random() * 1000),
+            rotate: 360,
+            duration: 18,
+            ease: "power1.inOut"
+        }, '<')
+    }, [])
+
+    return (
+        <div className='shape-background'>
+            <Image id='blob1' className='shape' src={blob} alt="blob 1" ></Image>
+            <Image id='blob2' className='shape' src={blob2} alt="blob 2" ></Image>
+            <Image id='blob3' className='shape' src={blob3} alt="blob 3" ></Image>
+            <Image id='blob4' className='shape' src={blob4} alt="blob 4" ></Image>
+        </div>
+    )
+}
