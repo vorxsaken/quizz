1f47ddfae7d57eb68d19d698580eb505d9a5f77d
vorxsaken <jhonyevans6326@gmail.com>
2023-03-03 19:20:02 +0700

    chore: make this component send timer coordinates to multiple component

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,21 +1,52 @@
 import Timer from "@/components/Timer"
 import FlyUpInOut from "@/animation/FlyUpInOut"
 import MultipleChoice from "@/components/MultipleChoice"
+import { useContext, useState } from "react"
+import { useIsomorphicEffect } from "@/animation/useIsomophicEffect";
+import { TransitionContext } from "@/context/TransitionContext";
+import FlyDownOut from "@/animation/FlyDownOut";
 
 export default function quiz() {
+    const [el, setEl] = useState();
+    const [data, dispatch] = useContext(TransitionContext)
+
+    useIsomorphicEffect(() => {
+        const elRect = document.getElementById('timer').getBoundingClientRect();
+        setEl(elRect);
+
+        return () => dispatch({ type: 'SET_GETOUT', getOut: false })
+    }, [])
+
     return (
         <div className="container">
-            <FlyUpInOut delayIn={0.5} className={"container container--title container__block text-white text-medium"}>
-                Gabut Quizz
-            </FlyUpInOut>
-            <FlyUpInOut delayIn={0.6} className={"container container--title container__block text-white text-medium"}>
-                what is the fastest animal in the world ?
-            </FlyUpInOut>
-            <FlyUpInOut delayIn={0.7} className="container container--row container__block text-white text-base">
-                <Timer />
-            </FlyUpInOut>
+            <FlyDownOut getOut={data.getOut}>
+                <FlyUpInOut
+                    delayIn={0.5}
+                    skipOutro={true}
+                    className={"container container--title container__block text-white text-medium"}>
+                    Gabut Quizz
+                </FlyUpInOut>
+            </FlyDownOut>
+            <FlyDownOut getOut={data.getOut}>
+                <FlyUpInOut
+                    delayIn={0.6}
+                    skipOutro={true}
+                    className={"container container__block text-white text-medium"}>
+                    what is the fastest animal in the world ?
+                </FlyUpInOut>
+            </FlyDownOut>
+            <span id="timer">
+                <FlyDownOut getOut={data.getOut}>
+                    <FlyUpInOut
+                        delayIn={0.7}
+                        skipOutro={true}
+                        className="container container--row container__block text-white text-base">
+                        <Timer />
+                    </FlyUpInOut>
+                </FlyDownOut>
+            </span>
             <div className="container container--row container__block">
-                <MultipleChoice />                
+                <MultipleChoice el={el} />
             </div>
         </div>
     )
