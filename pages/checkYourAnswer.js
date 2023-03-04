6961d5fd5322ab2bd5fc8ac32fbf63aa7760c265
vorxsaken <jhonyevans6326@gmail.com>
2023-03-04 19:55:02 +0700

    feat: now checker card can flip in y axis

--- pages/checkYourAnswer.js
+++ pages/checkYourAnswer.js
@@ -1,45 +1,56 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { TransitionContext } from '@/context/TransitionContext'
 import { useIsomorphicEffect } from '@/animation/useIsomophicEffect';
 import { gsap } from 'gsap';
+import CheckAnswCard from '@/components/CheckAnswCard';
+import CheckIcon from '../assets/check.svg';
+import CrossIcon from '../assets/cross.png';
 
 function checkYourAnswer() {
     const [data] = useContext(TransitionContext);
     const getLastData = data.choosed[data.choosed.length - 1];
+    const flipCardTl = gsap.timeline();
 
     useIsomorphicEffect(() => {
-        gsap.set('#card', {
+        gsap.set('#cardCont', {
+            transformStyle: 'preserve-3d',
+            transformPerspective: 1000
+        })
+
+        gsap.set('#cardFront', {
             x: getLastData.position[0] + 30,
             y: getLastData.position[1],
-            scale: 1.4
+            scale: 1.4,
         })
+
+        gsap.set('#cardBack', {
+            x: getLastData.position[0] + 30,
+            y: getLastData.position[1],
+            scale: 1.4,
+            rotationY: -180
+        })
+
+        flipCardTl.to('#cardFront', {duration: 1.3, rotationY: 180, ease: 'back.inOut'})
+        flipCardTl.to('#cardBack', {duration: 1.3, rotationY: 0, ease: 'back.inOut'}, 0)
+
     })
 
     return (
-        <div className='container' style={{ position: 'fixed' }}>
-            <div
-                id='card'
-                className='container__block__base-rect'
-                style={{
-                    position: 'absolute',
-                    top: '0px',
-                    left: '0px',
-                    height: getLastData.size[1],
-                    width: getLastData.size[0] - 61
-                }}
-            >
-                <div className='container__block__rect'>
-                    <div className='container container__block text-small'>
-                        <div
-                            className='container__block__multiple-choice'
-                            style={{ backgroundColor: getLastData.bgColor }}
-                        >
-                            {getLastData.answear[0]}
-                        </div>
-                        <span>{getLastData.answear[1]}</span>
-                    </div>
-                </div>
-            </div>
+        <div id='cardCont' className='container' style={{ position: 'fixed' }}>
+            <CheckAnswCard
+                id={'cardFront'}
+                width={getLastData.size[0] - 61}
+                height={getLastData.size[1]}
+                bgColor={getLastData.bgColor}
+                alpha={getLastData.answear[0]}
+                answer={getLastData.answear[1]}
+            />
+            <CheckAnswCard
+                id={'cardBack'}
+                width={getLastData.size[0] - 61}
+                height={getLastData.size[1]}
+                icon={CrossIcon}
+            />
         </div>
     )
 }
