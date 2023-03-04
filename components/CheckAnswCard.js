6d3e1049707d3e9b110bfdc6be5c2e0ff40a4180
vorxsaken <jhonyevans6326@gmail.com>
2023-03-04 19:53:37 +0700

    feat: create seperated check answer component

---
+++ components/CheckAnswCard.js
@@ -0,0 +1,46 @@
+import Image from "next/image"
+
+function CheckAnswCard({ id, height, width, bgColor, alpha, answer, icon }) {
+    return (
+        <div
+            id={id}
+            className='container__block__base-rect container__block_base-rect--flipable'
+            style={{
+                position: 'absolute',
+                top: '0px',
+                left: '0px',
+                height: height,
+                width: width,
+            }}
+        >
+            <div className='container__block__rect'>
+                <div className={`container container__block ${icon ? '' : 'text-small'}`}>
+                    {icon ? (
+                        <Image
+                            src={icon}
+                            loading='lazy'
+                            alt="correct"
+                            width={70}
+                            height={70}
+                        />
+                    ) : (
+                        <>
+                            <div
+                                className='container__block__multiple-choice'
+                                style={{ backgroundColor: bgColor }}
+                            >
+                                {alpha}
+                            </div>
+                            <span>{answer}</span>
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+CheckAnswCard.defaultProps = {
+    bgColor: 'white'
+}
+export default CheckAnswCard
