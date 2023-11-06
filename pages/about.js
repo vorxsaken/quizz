2c41b4da009f68dcfe9a2adbe0ae1e2a18d1f8e6
Alfahmi <96326848+vorxsaken@users.noreply.github.com>
2023-11-06 03:34:51 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -9,14 +9,14 @@ function About() {
                 About
             </FlyUpInOut>
             <FlyUpInOut delayIn={0.2} delayOut={0.7} className="container container--max-width-md container__card text-center">
-                Welcome to my Quiz App&#33; my app is designed to provide users with an engaging and enjoyable experience&#46;
-                i offer a wide range of quizzes that cover different topics&#44; from general knowledge to pop culture&#44; so
-                there&#39;s something for everyone&#46;
+                {`This application was created using Next JS and Greensock JavaScript library, the quiz data source for this application
+                comes from an external API that I found on the internet, you can see the source code for this application on my GitHub account
+                if you are curious about how all these animations are created and work.`}
             </FlyUpInOut>
             <FlyUpInOut delayIn={0.4} delayOut={0.5} className="container container--max-width-md container__card text-center">
-                At this Quiz App&#44; i committed to providing high-quality content that is both informative and entertaining&#46;
-                this quizzes are created by quiz api i found on internet&#44; that i adjust to personal taste&#46;
-                heve fun while learning new things
+                {`I would really appreciate if you contributed to the development of this application, in particular
+                I really want this application to have its own API for generating quizzes, and there is a categorization of 
+                topics that users can choose`}
             </FlyUpInOut>
             <FlyUpInOut delayOut={0.3} delayIn={0.6} className="container container--max-width-md container--row container__card">
                 <Scale delayOut={0.1} delayIn={0.9} className='text-xl'>
